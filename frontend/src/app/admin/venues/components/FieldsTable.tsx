f4c19a4a3902ff79b8a612d30ef5f322f6501caa
ztzrk <ralif877@gmail.com>
2025-12-08 19:19:18 +0800

    feat: Implement pagination in AllFieldsTable for improved field management

--- frontend/src/app/admin/venues/components/FieldsTable.tsx
+++ frontend/src/app/admin/venues/components/FieldsTable.tsx
@@ -1,15 +1,34 @@
 "use client";
-
+import { useState } from "react";
 import { columns as fieldColumns } from "./columns-fields";
 import { DataTable } from "@/components/shared/DataTable";
 import { useGetAllFields } from "@/hooks/useFields";
 import { FullScreenLoader } from "@/components/FullScreenLoader";
+import { PaginationState } from "@tanstack/react-table";
 
 export function AllFieldsTable() {
-    const { data: fields, isLoading, isError } = useGetAllFields();
+    const [{ pageIndex, pageSize }, setPagination] =
+        useState<PaginationState>({
+            pageIndex: 0,
+            pageSize: 10,
+        });
+    const { data, isLoading, isError } = useGetAllFields(
+        pageIndex + 1,
+        pageSize
+    );
 
     if (isLoading) return <FullScreenLoader />;
     if (isError) return <p className="text-red-500">Error loading fields.</p>;
 
-    return <DataTable columns={fieldColumns} data={fields || []} />;
+    const pageCount = data?.meta?.totalPages ?? 0;
+
+    return (
+        <DataTable
+            columns={fieldColumns}
+            data={data?.data || []}
+            pageCount={pageCount}
+            pagination={{ pageIndex, pageSize }}
+            onPaginationChange={setPagination}
+        />
+    );
 }
