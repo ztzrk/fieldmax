5289df9f4d94da94aa88e4a3c3c934c04cc65b8f
ztzrk <ralif877@gmail.com>
2025-12-08 19:19:59 +0800

    feat: Update getAll method in SportTypeService to support pagination and return validated data

--- frontend/src/services/sportType.service.ts
+++ frontend/src/services/sportType.service.ts
@@ -2,38 +2,44 @@ import { api } from "@/lib/api";
 import { AxiosError } from "axios";
 
 const SportTypeService = {
-    getAll: async () => {
+    getAll: async (page: number, limit: number) => {
         try {
-            const response = await api.get("/sport-types");
-            return response.data.data;
+            const response = await api.get("/sport-types", {
+                params: { page, limit },
+            });
+            return response.data;
         } catch (error) {
             throw error as AxiosError;
         }
     },
     create: async (data: { name: string; iconName?: string }) => {
         try {
-            await api.post("/sport-types", data);
+            const response = await api.post("/sport-types", data);
+            return response.data;
         } catch (error) {
             throw error as AxiosError;
         }
     },
     update: async (id: string, data: { name: string; iconName?: string }) => {
         try {
-            await api.put(`/sport-types/${id}`, data);
+            const response = await api.put(`/sport-types/${id}`, data);
+            return response.data;
         } catch (error) {
             throw error as AxiosError;
         }
     },
     delete: async (id: string) => {
         try {
-            await api.delete(`/sport-types/${id}`);
+            const response = await api.delete(`/sport-types/${id}`);
+            return response.data;
         } catch (error) {
             throw error as AxiosError;
         }
     },
     deleteMultiple: async (ids: string[]) => {
         try {
-            await api.post("/sport-types/multiple", { ids });
+            const response = await api.post("/sport-types/multiple", { ids });
+            return response.data;
         } catch (error) {
             throw error as AxiosError;
         }
