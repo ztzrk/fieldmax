46cf3e6f155fc697835c7e79a7eb7a59b24c2066
ztzrk <ralif877@gmail.com>
2025-12-09 12:29:10 +0800

    feat: Refactor imports and streamline error handling middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,24 +2,22 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
 dotenv.config();
-
-import express, { Express, Request, Response, NextFunction } from "express";
-import { AuthRoute } from "./auth/auth.route";
-import { UsersRoute } from "./users/users.route";
-
 import cors from "cors";
+import express, { Express, Request, Response } from "express";
+import { errorMiddleware } from "./middleware/error.middleware";
+import { logger } from "./utils/logger";
 import cookieParser from "cookie-parser";
-import { SportTypesRoute } from "./sport-types/sport-types.route";
-import { VenuesRoute } from "./venues/venues.route";
-import { FieldsRoute } from "./fields/fields.route";
-import { UploadsRoute } from "./uploads/uploads.route";
-import { RenterRoute } from "./renter/renter.route";
-import { ChatRoute } from "./chat/chat.route";
-import { ProfileRoute } from "./profile/profile.route";
+import { AuthRoute } from "./auth/auth.route";
 import { BookingsRoute } from "./bookings/bookings.route";
+import { ChatRoute } from "./chat/chat.route";
+import { FieldsRoute } from "./fields/fields.route";
 import { PaymentsRoute } from "./payments/payments.route";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-import { CustomError, ValidationError } from "./utils/errors";
+import { ProfileRoute } from "./profile/profile.route";
+import { RenterRoute } from "./renter/renter.route";
+import { SportTypesRoute } from "./sport-types/sport-types.route";
+import { UploadsRoute } from "./uploads/uploads.route";
+import { UsersRoute } from "./users/users.route";
+import { VenuesRoute } from "./venues/venues.route";
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
@@ -59,44 +57,12 @@ app.use("/api", profileRoute.router);
 app.use("/api", bookingsRoute.router);
 app.use("/api", paymentsRoute.router);
 
-// Simple test route
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to FIELDMAX API! 🚀");
 });
 
-// Error handling middleware
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack); // Log the error for debugging
+app.use(errorMiddleware);
 
-    let statusCode = 500;
-    let message = "Something went wrong!";
-
-    if (err instanceof CustomError) {
-        statusCode = err.statusCode;
-        message = err.message;
-        if (err instanceof ValidationError && err.errors) {
-            return res.status(statusCode).json({ errors: err.errors });
-        }
-    } else if (err instanceof PrismaClientKnownRequestError) {
-        if (err.code === "P2002") {
-            statusCode = 409; // Conflict
-            const target = Array.isArray(err.meta?.target)
-                ? err.meta?.target.join(", ")
-                : err.meta?.target || "field";
-            message = `The ${target} already exists.`;
-        } else {
-            statusCode = 400; // Bad Request for other known Prisma client errors
-            message = err.message;
-        }
-    } else if (err.message) {
-        statusCode = 400; // Assume bad request if a specific error message is provided
-        message = err.message;
-    }
-
-    res.status(statusCode).json({ message });
-});
-
-// Start the server
 app.listen(PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+    logger.info(`[server]: Server is running at http://localhost:${PORT}`);
 });
