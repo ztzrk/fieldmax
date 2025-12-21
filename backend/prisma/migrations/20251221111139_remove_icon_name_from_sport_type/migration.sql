d5d05f2d6fa98d0e12e637ad7bdcdb764e1b54d0
ztzrk <ralif877@gmail.com>
2025-12-21 19:26:56 +0800

    feat: Add migration script to drop icon_name column from sport_types table

---
+++ backend/prisma/migrations/20251221111139_remove_icon_name_from_sport_type/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `icon_name` on the `sport_types` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "sport_types" DROP COLUMN "icon_name";
