fc0a7c50ad70fa51c1c4e2bcab90e859cdcf7259
ztzrk <ralif877@gmail.com>
2025-12-21 19:29:00 +0800

    feat: Add CreateFieldButton component for creating new fields

---
+++ frontend/src/components/shared/fields/CreateFieldButton.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { useCreateField } from "@/hooks/useFields";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { FieldForm } from "./FieldForm";
+import { FieldFormValues } from "@/lib/schema/field.schema";
+
+interface CreateFieldButtonProps {
+    venueId: string;
+}
+
+export function CreateFieldButton({ venueId }: CreateFieldButtonProps) {
+    const [isOpen, setIsOpen] = useState(false);
+    const { mutate: createField, isPending } = useCreateField();
+
+    const handleSubmit = (values: FieldFormValues) => {
+        createField(
+            { ...values, venueId: venueId },
+            {
+                onSuccess: () => {
+                    setIsOpen(false);
+                },
+            }
+        );
+    };
+
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+                <Button>Create Field</Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create New Field</DialogTitle>
+                </DialogHeader>
+                <FieldForm onSubmit={handleSubmit} isPending={isPending} />
+            </DialogContent>
+        </Dialog>
+    );
+}
