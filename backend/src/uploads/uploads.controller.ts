91042c15a41370f0adbf20f681428a51fa466f65
ztzrk <ralif877@gmail.com>
2025-12-21 19:27:19 +0800

    feat: Remove Supabase client import from uploads controller

--- backend/src/uploads/uploads.controller.ts
+++ backend/src/uploads/uploads.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { supabase } from "../lib/supabase";
+
 import { VenuesService } from "../venues/venues.service";
 import { FieldsService } from "../fields/fields.service";
 
