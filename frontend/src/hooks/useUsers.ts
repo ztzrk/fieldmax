3775faaf5323d5cc0dc3947df4d0ec57aa492db7
ztzrk <ralif877@gmail.com>
2025-12-22 18:41:11 +0800

    feat: Update useGetAllUsers to include search functionality and maintain previous data

--- frontend/src/hooks/useUsers.ts
+++ frontend/src/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient, keepPreviousData } from "@tanstack/react-query";
 import UserService from "@/services/user.service";
 import { toast } from "sonner";
 import {
@@ -9,13 +9,14 @@ import {
 import { AxiosError } from "axios";
 import { BackendErrorResponse } from "@/types/error";
 
-export function useGetAllUsers(page: number, limit: number) {
+export function useGetAllUsers(page: number, limit: number, search?: string) {
     return useQuery({
-        queryKey: ["users", { page, limit }],
+        queryKey: ["users", { page, limit, search }],
         queryFn: async () => {
-            const data = await UserService.getAllUsers(page, limit);
+            const data = await UserService.getAllUsers(page, limit, search);
             return usersPaginatedApiResponseSchema.parse(data);
         },
+        placeholderData: keepPreviousData,
     });
 }
 
